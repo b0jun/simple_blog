31f51501ba5c535e13438ffdedb23a833809a375
billowycloud <bjkim0228@naver.com>
2020-11-24 17:30:52 +0900

    set deploy

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ import helmet from 'helmet';
 import hpp from 'hpp';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import path from 'path';
 import morgan from 'morgan';
 import config from './config';
 import postRoutes from './routes/api/post';
@@ -12,6 +13,7 @@ import searchRoutes from './routes/api/search';
 
 const app = express();
 const { MONGO_URI } = config;
+const prod = process.env.NODE_ENV === 'production';
 
 app.use(hpp());
 app.use(helmet());
@@ -34,4 +36,12 @@ app.use('/api/post', postRoutes);
 app.use('/api/user', userRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/search', searchRoutes);
+
+//프론트주소는 아래를 통해 받음
+if (prod) {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+  });
+}
 export default app;
